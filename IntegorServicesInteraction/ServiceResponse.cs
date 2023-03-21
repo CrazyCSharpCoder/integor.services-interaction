91b546cfa1bc3154dd29ccb006d3c4fdf76cf5db
Stephen L <logunov.stepan2001@gmail.com>
2023-03-22 01:28:28 +0700

    Add status code and fixed universal response ctor

--- IntegorServicesInteraction/ServiceResponse.cs
+++ IntegorServicesInteraction/ServiceResponse.cs
@@ -10,33 +10,39 @@ namespace IntegorServicesInteraction
 {
 	public class ServiceResponse<TResponseObject> where TResponseObject : class
 	{
+		public int StatusCode { get; }
 		public Dictionary<string, string> Cookies { get; }
 
 		public TResponseObject? Value { get; }
 		public IResponseError[]? Errors { get; }
 
-        public ServiceResponse()
+        public ServiceResponse(int statusCode)
         {
+			StatusCode = statusCode;
 			Cookies = new Dictionary<string, string>();
         }
 
-        public ServiceResponse(TResponseObject value, Dictionary<string, string> cookies)
+        public ServiceResponse(int statusCode, TResponseObject value, Dictionary<string, string> cookies)
 		{
+			StatusCode = statusCode;
 			Value = value ?? throw new ArgumentNullException(nameof(value));
 			Cookies = cookies ?? throw new ArgumentNullException(nameof(cookies));
 		}
 
-        public ServiceResponse(IEnumerable<IResponseError> errors, Dictionary<string, string> cookies)
+        public ServiceResponse(int statusCode, IEnumerable<IResponseError> errors, Dictionary<string, string> cookies)
 		{
+			StatusCode = statusCode;
 			Errors = errors?.ToArray() ?? throw new ArgumentNullException(nameof(errors));
 			Cookies = cookies ?? throw new ArgumentNullException(nameof(cookies));
 		}
 
-        public ServiceResponse(TResponseObject value) : this(value, new Dictionary<string, string>())
+        public ServiceResponse(int statusCode, TResponseObject value)
+			: this(statusCode, value, new Dictionary<string, string>())
         {
         }
 
-		public ServiceResponse(IEnumerable<IResponseError> errors) : this(errors, new Dictionary<string, string>())
+		public ServiceResponse(int statusCode, IEnumerable<IResponseError> errors)
+			: this(statusCode, errors, new Dictionary<string, string>())
 		{
 		}
 	}
--- IntegorServicesInteraction/UniversalServiceResponse.cs
+++ IntegorServicesInteraction/UniversalServiceResponse.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IntegorErrorsHandling;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,28 @@ namespace IntegorServicesInteraction
 {
 	public class UniversalServiceResponse : ServiceResponse<object>
 	{
+		public UniversalServiceResponse(int statusCode) : base(statusCode)
+		{
+		}
+
+		public UniversalServiceResponse(int statusCode, object value, Dictionary<string, string> cookies)
+			: base(statusCode, value, cookies)
+		{
+		}
+
+		public UniversalServiceResponse(int statusCode, IEnumerable<IResponseError> errors, Dictionary<string, string> cookies)
+			: base(statusCode, errors, cookies)
+		{
+		}
+
+		public UniversalServiceResponse(int statusCode, object value)
+			: base(statusCode, value)
+		{
+		}
+
+		public UniversalServiceResponse(int statusCode, IEnumerable<IResponseError> errors)
+			: base(statusCode, errors)
+		{
+		}
 	}
 }
